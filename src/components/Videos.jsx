d5de96ee2afaea197c8c08373580c54c06976b04
Zahra Naseri <zahranaseri593@gmail.com>
2023-05-27 20:01:13 +0330

    fetching the videos and rendering them to the screen

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { Stack,Box } from "@mui/material"
 
-export default function Videos () {
+import ChannelCard  from "./ChannelCard"
+import VideoCard from "./VideoCard"
+
+
+export default function Videos ({videos}) {
+  console.log(videos)
+  
   return (
-    <div>Videos</div>
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, idx)=>(
+        <Box key={idx}>
+           {item.id.videoId && <VideoCard video={item} />}
+           {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
+        </Box>
+      ))}
+    </Stack>
   )
 }
