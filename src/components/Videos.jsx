197483b7bc08f0b0c21983ba7131524375d14c97
Zahra Naseri <zahranaseri593@gmail.com>
2023-05-28 14:17:37 +0330

    channel page

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,6 @@
 import { Stack,Box } from "@mui/material"
 
-import ChannelCard  from "./ChannelCard"
-import VideoCard from "./VideoCard"
-
+import {ChannelCard, VideoCard}  from "./"
 
 export default function Videos ({videos}) {
   console.log(videos)
